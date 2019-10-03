86203e00c21a79eb15a35cd10de06c88f3f835dc
ZachYoung12 <55261789+ZachYoung12@users.noreply.github.com>
2019-10-03 18:12:51 -0500

    Player Movement
    Added the ability for the player to move by taking in player input

---
+++ Ice Cold Beer/Assets/Scripts/MovingRampSupport.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingRampSupport : MonoBehaviour
+{
+    private float moveSpeed = 0.05f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("this is a test.");
+        //transform.Translate(5, 0, 0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //If the player presses the up arrow, we move the square up.
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, moveSpeed, 0);
+        }
+        //If the player presses the down arrow, we move the square down.
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -1 * moveSpeed, 0);
+        }
+    }
+}
