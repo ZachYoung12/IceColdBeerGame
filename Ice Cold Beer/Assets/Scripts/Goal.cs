a076ed0e9ea1afc1f202e7f1e8729ff4571dc653
ZachYoung12 <55261789+ZachYoung12@users.noreply.github.com>
2019-10-24 18:07:28 -0500

    Added a goal and text
    Added a goal for the player to interact with using a trigger and added text that will eventually appear when the player touches the goal

---
+++ Ice Cold Beer/Assets/Scripts/Goal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//detects when the ball touches the goal, and then win a level
+
+public class Goal : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Touched trigger");
+        if (collision.tag == "Player")
+        {
+            Debug.Log("The player has entered the goal");
+        }
+    }
+}
