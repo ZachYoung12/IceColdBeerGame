2f4512fea060354a57ac9c0eaf235f4b04c7d2af
ZachYoung12 <55261789+ZachYoung12@users.noreply.github.com>
2019-11-07 17:57:37 -0600

    Added Sound
    Added sound to the interaction with the goal and background music. Also changed the collision size of the goal to make it harder to win.

--- Ice Cold Beer/Assets/Scripts/Goal.cs
+++ Ice Cold Beer/Assets/Scripts/Goal.cs
@@ -6,11 +6,20 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+    public GameObject winText;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Touched trigger");
         if (collision.tag == "Player")
         {
+            winText.SetActive(true);
+            audioSource.Play();
             Debug.Log("The player has entered the goal");
         }
     }
